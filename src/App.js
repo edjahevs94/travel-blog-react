b00f428be7db03ef98dcab78dcd727e5e8d56acc
Edgard <edjahevs@gmail.com>
2022-06-08 20:55:15 -0500

    setting the practice

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@
 
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
-import data from "./data.js";
+
 
 function App() {
-  const cards = data.map(item => {return(
-    <Card
-      key = {item.id}
-      item = {item}
-     />
-  )})
+  
   return (
     <div className="App">
       <Navbar />
-      {cards}
+      <Card />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,22 @@
 import React from "react"
 import pointer from "../images/location.png"
 
-export default function Card(props) {
+export default function Card() {
     return(
         <div className="card">
-            <img className="card--img" src={props.item.imageUrl} alt="card-foto" />
+            <img className="card--img" src="https://source.unsplash.com/WLxQvbMyfas" alt="card-foto" />
             <div className="card--stats">
                 <div className="card--location">
                     <img src={pointer} className="card--location--icon" alt="pointer"/>
-                    <h5>{props.item.location}</h5>
-                    <a href={props.item.googleMapsUrl}>View on google maps</a>
+                    <h5>Japan</h5>
+                    <a href="https://source.unsplash.com/WLxQvbMyfas">View on google maps</a>
                 </div>
-                <p className="card--title">{props.item.title}</p>
+                <p className="card--title">Mount Fuji</p>
                 <div className="card--date">
-                    <h4>{props.item.startDate}  -</h4>
-                    <h4>{props.item.endDate}</h4>
+                    <h4>12 Jan, 2021  -</h4>
+                    <h4>24 Jan, 2021</h4>
                 </div>
-                <p className="card--info">{props.item.description}</p>
+                <p className="card--info">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
                 
             </div>
             
